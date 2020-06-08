e5dfe50b17aad787c86b049727953931ace21704
Damian <damian@autistici.org>
2020-06-09 00:34:57 +0200

    Added security camera

---
+++ Content.Client/GameObjects/Components/Security/SecurityCameraVisualizer2D.cs
@@ -0,0 +1,9 @@
+﻿using Robust.Client.GameObjects;
+
+namespace Content.Client.GameObjects.Components.Security
+{
+    public class SecurityCameraVisualizer2D : AppearanceVisualizer
+    {
+
+    }
+}
---
+++ Content.Server/GameObjects/Components/Security/SecurityCameraComponent.cs
@@ -0,0 +1,10 @@
+﻿using Robust.Shared.GameObjects;
+
+namespace Content.Server.GameObjects.Components.Security
+{
+    [RegisterComponent]
+    public class SecurityCameraComponent : Component
+    {
+        public override string Name => "SecurityCamera";
+    }
+}
