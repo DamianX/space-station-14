92ee81c17b3e7046ea72c7417eb553afed1722de
Damian <damian@autistici.org>
2020-07-31 16:18:46 +0200

    Create one DbContext per operation

--- Content.Server.Database/PrefsDb.cs
+++ Content.Server.Database/PrefsDb.cs
@@ -6,23 +6,26 @@ namespace Content.Server.Database
 {
     public class PrefsDb
     {
-        private readonly PreferencesDbContext _prefsCtx;
+        private readonly IDatabaseConfiguration _dbConfig;
 
-        public PrefsDb(IDatabaseConfiguration dbConfig)
-        {
-            _prefsCtx = dbConfig switch
+        private PreferencesDbContext PrefsCtx =>
+            _dbConfig switch
             {
-                SqliteConfiguration sqlite => (PreferencesDbContext) new SqlitePreferencesDbContext(
+                SqliteConfiguration sqlite => new SqlitePreferencesDbContext(
                     sqlite.Options),
                 PostgresConfiguration postgres => new PostgresPreferencesDbContext(postgres.Options),
                 _ => throw new NotImplementedException()
             };
-            _prefsCtx.Database.Migrate();
+
+        public PrefsDb(IDatabaseConfiguration dbConfig)
+        {
+            _dbConfig = dbConfig;
+            PrefsCtx.Database.Migrate();
         }
 
         public Prefs GetPlayerPreferences(string username)
         {
-            return _prefsCtx
+            return PrefsCtx
                 .Preferences
                 .Include(p => p.HumanoidProfiles)
                 .ThenInclude(h => h.Jobs)
@@ -31,21 +34,26 @@ namespace Content.Server.Database
 
         public void SaveSelectedCharacterIndex(string username, int slot)
         {
-            var prefs = _prefsCtx.Preferences.SingleOrDefault(p => p.Username == username);
+            var prefs = PrefsCtx.Preferences.SingleOrDefault(p => p.Username == username);
             if (prefs is null)
-                _prefsCtx.Preferences.Add(new Prefs
+            {
+                PrefsCtx.Preferences.Add(new Prefs
                 {
                     Username = username,
                     SelectedCharacterSlot = slot
                 });
+            }
             else
+            {
                 prefs.SelectedCharacterSlot = slot;
-            _prefsCtx.SaveChanges();
+            }
+
+            PrefsCtx.SaveChanges();
         }
 
         public void SaveCharacterSlot(string username, HumanoidProfile newProfile)
         {
-            var prefs = _prefsCtx
+            var prefs = PrefsCtx
                 .Preferences
                 .Single(p => p.Username == username);
             var oldProfile = prefs
@@ -53,17 +61,17 @@ namespace Content.Server.Database
                 .SingleOrDefault(h => h.Slot == newProfile.Slot);
             if (!(oldProfile is null)) prefs.HumanoidProfiles.Remove(oldProfile);
             prefs.HumanoidProfiles.Add(newProfile);
-            _prefsCtx.SaveChanges();
+            PrefsCtx.SaveChanges();
         }
 
         public void DeleteCharacterSlot(string username, int slot)
         {
-            var profile = _prefsCtx
+            PrefsCtx
                 .Preferences
                 .Single(p => p.Username == username)
                 .HumanoidProfiles
                 .RemoveAll(h => h.Slot == slot);
-            _prefsCtx.SaveChanges();
+            PrefsCtx.SaveChanges();
         }
     }
 }
